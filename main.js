6349cabcdbdcd6042c2737b954d9fa8ce95e6279
Chris Molina <ragepool123@gmail.com>
2023-10-13 09:19:25 +0800

    feat: add my skills header

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import "./src/header.css";
+import "./src/components/heading.css";
 import "./src/hero.css";
 import "./src/about.css";
+import "./src/css/skills.css";
 import './style.css';
 import "./src/modern-normalize.css";
 
