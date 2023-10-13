94573b38fdf82be1e671327c0e897624831ac5af
Chris Molina <ragepool123@gmail.com>
2023-10-14 05:00:07 +0800

    feat: add project section and button utility

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 import "./styles/components/header.css";
-import "./styles/utils/heading.css";
 import "./styles/components/hero.css";
 import "./styles/components/about.css";
 import "./styles/components/skills.css";
+import "./styles/components/projects.css";
+import "./styles/utils/heading.css";
+import "./styles/utils/button.css";
 import './styles/style.css';
 import "./styles/modern-normalize.css";
 
